8c74047b57029aa66661a141a72df1e73e18b8ab
Admin <Admin@DESKTOP-6PQE64G>
2024-01-16 09:23:42 +0530

    Add project files.

---
+++ Stock price change/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Stock
+{
+    // Properties
+    public string StockName { get; set; }
+    public string StockSymbol { get; set; }
+    public double PreviousClosingPrice { get; set; }
+    public double CurrentClosingPrice { get; set; }
+
+    // Constructor
+    public Stock(string name, string symbol, double previousPrice, double currentPrice)
+    {
+        StockName = name;
+        StockSymbol = symbol;
+        PreviousClosingPrice = previousPrice;
+        CurrentClosingPrice = currentPrice;
+    }
+
+    // Method to calculate percentage change
+    public double GetChangePercentage()
+    {
+        return (CurrentClosingPrice - PreviousClosingPrice) / PreviousClosingPrice * 100;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var stock = new Stock("WIPRO Technologies", "WIPRO", 480.00, 495.00);
+        Console.WriteLine($"The WIPRO stock percentage change from previous day is: {stock.GetChangePercentage()}%");
+        Console.ReadKey();
+    }
+    
+}
